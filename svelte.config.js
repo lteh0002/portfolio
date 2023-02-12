44489ca3ca9e3482940b25555cbef465fd0afc07
lteh0002 <tehlinhui@gmail.com>
2023-02-12 13:55:46 +0800

    Set adapter to default

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  adapter: adapter({
-    // default options are shown. On some platforms
-    // these options are set automatically — see below
-    pages: 'build',
-    assets: 'build',
-    fallback: null,
-    precompress: false,
-    strict: true,
-  }),
+  kit: {
+    adapter: adapter()
+  },
   preprocess: vitePreprocess()
 };
 
