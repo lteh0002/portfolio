62605513439f0ee253f6f1d7cb9a8edef45d668d
lteh0002 <tehlinhui@gmail.com>
2023-02-12 13:25:53 +0800

    Edit netlify.toml value

--- svelte.config.js
+++ svelte.config.js
@@ -7,11 +7,11 @@ const config = {
   adapter: adapter({
     // default options are shown. On some platforms
     // these options are set automatically — see below
-    // pages: 'build',
-    // assets: 'build',
-    // fallback: null,
-    // precompress: false,
-    // strict: true,
+    pages: 'build',
+    assets: 'build',
+    fallback: null,
+    precompress: false,
+    strict: true,
     edge: false,
     split: false
   }),
