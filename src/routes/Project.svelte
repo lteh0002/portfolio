d7ac35232b03b6ed5a09f710c8d2b78592b6e7a0
lteh0002 <tehlinhui@gmail.com>
2023-02-12 13:13:06 +0800

    Edit svelte.config for netlify hosting

--- src/routes/Project.svelte
+++ src/routes/Project.svelte
@@ -4,7 +4,7 @@
   
   <div class="hidden sm:hidden lg:relative lg:flex lg:justify-between lg:items-center">
     <div>
-      <div class="flex">
+      <div class="flex"> 
         <h1 class="text-[4vw] mr-[1vw]">Next Jobs DB</h1>
         <a href="https://github.com/lteh0002/svelte-jobsite"><img class="w-[5vw] inline hover:-translate-y-3 hover:duration-300 hover:cursor-pointer" src="github.webp" alt=""></a>
       </div>
@@ -110,19 +110,19 @@
         <a href="#slide3" class="btn btn-circle">❯</a>
       </div>
     </div> 
-    <!-- <div id="slide3" class="carousel-item relative w-full">
-      <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" class="w-full" />
+    <div id="slide3" class="carousel-item relative w-full">
+      <img src="rockpaperscissor.png" class="w-full" alt="test"/>
       <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
         <a href="#slide2" class="btn btn-circle">❮</a> 
         <a href="#slide4" class="btn btn-circle">❯</a>
       </div>
     </div> 
     <div id="slide4" class="carousel-item relative w-full">
-      <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" class="w-full" />
+      <img src="rockpaperscissor.png" class="w-full" alt="test"/>
       <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
         <a href="#slide3" class="btn btn-circle">❮</a> 
         <a href="#slide1" class="btn btn-circle">❯</a>
       </div>
-    </div> -->
+    </div>
   </div>
   </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	adapter: adapter({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: 'build',
-		assets: 'build',
-		fallback: null,
-		precompress: false,
-		strict: true
-	  }),
-  preprocess: vitePreprocess()
+  adapter: adapter({
+    // default options are shown. On some platforms
+    // these options are set automatically — see below
+    // pages: 'build',
+    // assets: 'build',
+    // fallback: null,
+    // precompress: false,
+    // strict: true,
+    edge: false,
+    split: false
+  }),
+  // preprocess: vitePreprocess()
 };
 
 export default config;
