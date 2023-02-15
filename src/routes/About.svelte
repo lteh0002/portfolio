c5f87ec9bb085ff52c909a8104e6b19e55e4c555
lteh0002 <tehlinhui@gmail.com>
2023-02-16 01:27:33 +0800

    Setting image sizing for carousel for sm 640px and below media query to be bigger

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,9 +1,13 @@
+<script>
+  import Carousel from "./Carousel.svelte";
+</script>
 <div id="aboutme" class="max-w-full px-[6vw] pt-[17vw] sm:pt-[4.5vw] md:pt-[2vw]">
 	<h1 class="text-[6vw]">About Me</h1>
 	<div class="sm:flex">
 		<div class="flex flex-col gap-3 text-justify">
 			<p class="text-[4.95vw] pt-[0.5vw] sm:text-[2.5vw] md:text-[2vw]">
-				Hey! I'm <strong><i>Lin Hui</i></strong>. Previously a Civil Engineer, now a full stack web developer!
+				Hey! I'm <strong><i>Lin Hui</i></strong>. Previously a Civil Engineer, now a full stack web
+				developer!
 			</p>
 			<p class="text-[4.95vw] sm:text-[2.5vw] md:text-[2vw]">
 				While I loved the problem-solving aspect of my previous field, I couldn't ignore my growing
@@ -22,34 +26,5 @@
 			</p> -->
 		</div>
 	</div>
-	<div class="flex gap-6 carousel carousel-end h-[17vw] lg:h-[14vw] mt-[3vw]">
-		<div class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[75%] w-[11vw]">
-			<img class="h-[75%]" src="javascript.webp" alt="Drink" />
-			<p class="text-[2.3vw]">Javascript</p>
-		</div>
-		<div class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[75%] w-[11vw]">
-			<img class="h-[75%]" src="html.webp" alt="Drink" />
-			<p class="text-[2.3vw] sm:text-[2vw]">HTML</p>
-		</div>
-		<div class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[75%] w-[11vw]">
-			<img class="h-[75%]" src="css-3-icon.webp" alt="Drink" />
-			<p class="text-[2.3vw] sm:text-[2vw]">CSS</p>
-		</div>
-		<div class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[75%] w-[11vw]">
-			<img class="h-[75%]" src="capacitorjs.webp" alt="Drink" />
-			<p class="text-[2.3vw] sm:text-[2vw]">CapacitorJS</p>
-		</div>
-		<div class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[75%] w-[11vw]">
-			<img class="h-[75%]" src="git.webp" alt="Drink" />
-			<p class="text-[2.3vw] sm:text-[2vw]">Git</p>
-		</div>
-		<div class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[75%] w-[11vw]">
-			<img class="h-[75%]" src="github.webp" alt="Drink" />
-			<p class="text-[2.3vw] sm:text-[2vw]">Github</p>
-		</div>
-		<div class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[75%] w-[11vw]">
-			<img class="h-[75%]" src="svelte.webp" alt="Drink" />
-			<p class="text-[2.3vw] sm:text-[2vw]">Svelte</p>
-		</div>
-	</div>
+  <Carousel/>
 </div>
---
+++ src/routes/Carousel.svelte
@@ -0,0 +1,44 @@
+<div class="flex gap-6 carousel carousel-end h-[20vw] sm:h-[17vw] lg:h-[14vw] mt-[3vw]">
+		<div
+			class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+		>
+			<img class="h-[75%]" src="javascript.webp" alt="Drink" />
+			<p class="text-[2.6vw]">Javascript</p>
+		</div>
+		<div
+			class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+		>
+			<img class="h-[75%]" src="html.webp" alt="Drink" />
+			<p class="text-[2.6vw] sm:text-[2vw]">HTML</p>
+		</div>
+		<div
+			class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+		>
+			<img class="h-[75%]" src="css-3-icon.webp" alt="Drink" />
+			<p class="text-[2.6vw] sm:text-[2vw]">CSS</p>
+		</div>
+		<div
+			class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+		>
+			<img class="h-[75%]" src="capacitorjs.webp" alt="Drink" />
+			<p class="text-[2.6vw] sm:text-[2vw]">CapacitorJS</p>
+		</div>
+		<div
+			class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+		>
+			<img class="h-[75%]" src="git.webp" alt="Drink" />
+			<p class="text-[2.6vw] sm:text-[2vw]">Git</p>
+		</div>
+		<div
+			class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+		>
+			<img class="h-[75%]" src="github.webp" alt="Drink" />
+			<p class="text-[2.6vw] sm:text-[2vw]">Github</p>
+		</div>
+		<div
+			class="flex flex-col items-center carousel-item border border-dotted p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+		>
+			<img class="h-[75%]" src="svelte.webp" alt="Drink" />
+			<p class="text-[2.6vw] sm:text-[2vw]">Svelte</p>
+	</div>
+</div>
