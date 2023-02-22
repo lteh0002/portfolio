d4160c35f0986f7506fcb5808ad34014bdd57d3a
lteh0002 <tehlinhui@gmail.com>
2023-02-23 00:58:10 +0800

    Change background color for carousel during dark mode

--- src/routes/Carousel.svelte
+++ src/routes/Carousel.svelte
@@ -1,3 +1,7 @@
+<script>
+
+</script>
+
 <style>
   .material-symbols-outlined {
     font-variation-settings:
@@ -12,58 +16,70 @@
 <div class="carousel carousel-end h-[20vw] sm:h-[17vw] lg:h-[13vw]">
   <div class="overflow-x-auto flex gap-6">
     <div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="javascript.webp" alt="Javascript" />
-			<p class="text-[2.6vw] sm:text-[2vw]">Javascript</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">Javascript</p>
 		</div>
 		<div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="html.webp" alt="HTML" />
-			<p class="text-[2.6vw] sm:text-[2vw]">HTML</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">HTML</p>
 		</div>
 		<div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="css-3-icon.webp" alt="CSS" />
-			<p class="text-[2.6vw] sm:text-[2vw]">CSS</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">CSS</p>
 		</div>
 		<div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="capacitorjs.webp" alt="CapacitorJS" />
-			<p class="text-[2.6vw] sm:text-[2vw]">CapacitorJS</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">CapacitorJS</p>
 		</div>
 		<div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="git.webp" alt="Git" />
-			<p class="text-[2.6vw] sm:text-[2vw]">Git</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">Git</p>
 		</div>
 		<div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="github.webp" alt="Github" />
-			<p class="text-[2.6vw] sm:text-[2vw]">Github</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">Github</p>
 		</div>
     <div
-    class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+    class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
   >
     <img class="h-[75%]" src="nodejs.webp" alt="NodeJS" />
-    <p class="text-[2.6vw] sm:text-[2vw]">NodeJS</p>
+    <p class="text-[2.6vw] sm:text-[2vw] text-black">NodeJS</p>
   </div>
 		<div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="svelte.webp" alt="Svelte" />
-			<p class="text-[2.6vw] sm:text-[2vw]">Svelte</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">Svelte</p>
 	</div>
   <div
-			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw]"
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
 		>
 			<img class="h-[75%]" src="pgsql.webp" alt="PostgreSQL" />
-			<p class="text-[2.6vw] sm:text-[2vw]">PostgreSQL</p>
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">PostgreSQL</p>
+	</div>
+  <div
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
+		>
+			<img class="h-[75%]" src="express.webp" alt="ExpressJS" />
+			<p class="text-[2.6vw] sm:text-[2vw] text-black">ExpressJS</p>
+	</div>
+  <div
+			class="snap-start flex flex-col items-center carousel-item border border-dotted border-[#BDBDBD] p-[1vw] h-[80%] w-[14vw] sm:w-[11vw] bg-white"
+		>
+			<img class="h-[75%]" src="prisma.png" alt="Prisma" />
+			<p class="text-[2.6vw] sm:text-[2vw] mb-[-1vw] text-black">Prisma</p>
 	</div>
   </div>
 </div>
--- src/routes/page.js
+++ src/routes/page.js
@@ -4,10 +4,7 @@ export function scrollToSection(id) {
 }
 
 export function scrollToTop() {
-//   const section = document.querySelectorAll(id)
-//   section.forEach(element => {
-//     element.scrollIntoView({ top: 0, behavior: 'smooth' });
-//   });
 window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
 
